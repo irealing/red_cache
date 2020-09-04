f704b9b6fdca87e13727661a425ef415cf51ed9a
Memory_Leak <irealing@163.com>
2020-09-04 18:05:37 +0800

    fixed: 修正pickle反序列化异常问题

--- red_cache/_base.py
+++ red_cache/_base.py
@@ -4,7 +4,7 @@ from typing import Callable, Any
 
 from ._cache import CacheOpt, CacheIt, _RmOptFactory
 from .typing import RedMapping, KeyType, _DecoratorFunc, TTL, Encoder, Decoder, json_encoder, json_decoder, _Func
-from .typing import pickle_encoder
+from .typing import pickle_encoder, pickle_decoder
 
 __author__ = 'Memory_Leak<irealing@163.com>'
 
@@ -39,7 +39,7 @@ class _BaseMapping(RedMapping, metaclass=abc.ABCMeta):
         return self.cache_it(key, ttl=ex)
 
     def pickle_cache(self, key: KeyType, ex: TTL = None):
-        return self.cache_it(key, ttl=ex, encoder=pickle_encoder, decoder=pickle_encoder)
+        return self.cache_it(key, ttl=ex, encoder=pickle_encoder, decoder=pickle_decoder)
 
     def remove(self, key: KeyType, by_return: bool = False):
         return self.remove_it(key, by_return=by_return)
